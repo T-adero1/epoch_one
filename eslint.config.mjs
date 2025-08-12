3ec7b1098d91cb949889908671ddb3794a25d977
Your Name <94543054+T-adero1@users.noreply.github.com>
2025-08-11 22:31:48 -0500

    lint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +15,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: parser,
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
     rules: {
       // Only keep absolutely critical errors
       "no-undef": "error",
